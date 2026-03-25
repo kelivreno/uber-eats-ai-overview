a48d93ea0393ada4dd49fb34d99ae30171a77537
kelivreno <joikonkazama@gmail.com>
2026-03-25 01:54:19 -0700

    Initialized repository

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from ubereatsaioverview!")
+import logging
+from fastapi import FastAPI
+import inngest
+import inngest.fast_api
+from dotenv import load_dotenv
+from inngest.experimental import ai
+import uuid
+import os
+import datetime
+
+load_dotenv()
+
+inngest_client = inngest.Inngest(
+    app_id="UE_AI_Overview",
+    logger=logging.getLogger("uvicorn"),
+    is_production=False,
+    serializer=inngest.PydanticSerializer()
+)
+
+@inngest_client.create_function(
+    fn_id="RAG: Ingest CSV",
+    trigger=inngest.TriggerEvent(event="rag/ingest.csv")
 
 
-if __name__ == "__main__":
-    main()
+)
+async def rag_ingest_csv(ctx: inngest.Context):
+    return {"hello":"world"}
+
+
+app = FastAPI()
+
+inngest.fast_api.serve(app,inngest_client, [])
--- overview.py
+++
@@ -1,6 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer('all-MiniLM-L6-v2')
-sentences = ["cheesy pizza", "Extra Large Meat Lovers - Whole Pie - $15.99"]
-embeddings = model.encode(sentences)
-print(embeddings.shape)
